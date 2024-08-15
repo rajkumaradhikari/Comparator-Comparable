c4013d3d29af06aac90e41dfdf4afa7e79dc7781
rajkumaradhikari <rajkumaradhikarime@gmail.com>
2024-08-16 04:09:45 +0545

    sort using comparator added / based on age

--- src/sortstudent/Main.java
+++ src/sortstudent/Main.java
@@ -1,6 +1,8 @@
 package sortstudent;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 class Student{
@@ -24,12 +26,25 @@ class Student{
 public class Main {
     public static void main(String[] args){
 
+        Comparator<Student > comparator = new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+               if (o1.age>o2.age){
+                   return 1;
+               }else{
+                   return -1;
+               }
+            }
+        };
+
         List<Student> students = new ArrayList<>();
 
         students.add(new Student("Rajkumar",25));
         students.add(new Student("Raj",22));
         students.add(new Student("Abin",30));
         students.add(new Student("Test",20));
+
+        Collections.sort(students,comparator);
 
        for (Student student : students){
            System.out.println(student);
